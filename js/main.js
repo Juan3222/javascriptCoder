dff7fc411176a4338bead0be9c15661ed4593ccf
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2022-07-30 09:38:50 -0300

    Commit de prueba

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let dia = "lunes";
+
+switch (dia) {
+  case "lunes":
+    console.log("Locro");
+    break;
+  case "Martes":
+    console.log("Arroz");
+    break;
+  case "Miercoles":
+    console.log("Hamburguesa");
+    break;
+  default:
+    console.log("No hay especiales");
+    break;
+}
