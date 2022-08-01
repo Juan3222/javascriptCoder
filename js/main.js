ea230791e24450980b483605ddb00d4b73e94302
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2022-08-01 17:46:36 -0300

    Agregado scss

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-let numero = prompt(
-  "Ingrese 1 para obtener cinco números pares por consola, u oprima 2 para obtener cinco números impares"
+let numero = parseInt(
+  prompt(
+    "Ingrese 1 para obtener cinco números pares por consola, u oprima 2 para obtener cinco números impares"
+  )
 );
 
-if (numero == 1) {
+if (numero === 1) {
   for (let i = 2; i <= 10; i = i + 2) {
     console.log(i);
   }
-} else if (numero == 2) {
+} else if (numero === 2) {
   for (let i = 1; i <= 10; i = i + 2) {
     console.log(i);
   }
