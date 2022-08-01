e5381d7a799ca590e1e9dda784030a5d18b9f86a
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2022-08-01 16:57:39 -0300

    Tarea complementaria: Crear algoritmo con un ciclo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-let dia = "lunes";
+let numero = prompt(
+  "Ingrese 1 para obtener cinco números pares por consola, u oprima 2 para obtener cinco números impares"
+);
 
-switch (dia) {
-  case "lunes":
-    console.log("Locro");
-    break;
-  case "Martes":
-    console.log("Arroz");
-    break;
-  case "Miercoles":
-    console.log("Hamburguesa");
-    break;
-  default:
-    console.log("No hay especiales");
-    break;
+if (numero == 1) {
+  for (let i = 2; i <= 10; i = i + 2) {
+    console.log(i);
+  }
+} else if (numero == 2) {
+  for (let i = 1; i <= 10; i = i + 2) {
+    console.log(i);
+  }
+} else {
+  alert("El dato ingresado no es correcto");
 }
