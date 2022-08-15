74ad0d2f6b7b732911c4d84c40a4386413bcc255
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2022-08-15 16:08:46 -0300

    Desafío de Arrays

--- js/main.js
+++ js/main.js
@@ -1,38 +1,105 @@
-producto1 = 455;
-producto2 = 50;
-producto3 = 400;
-producto4 = 2500;
-producto5 = 350;
-carrito = 0;
-comprar = 0;
-efectivo = 5000;
-
-alert('Bienvenido a la tienda "Cinco Productos", presione del 1 al 5 para seleccionar el producto deseado');
-
-while (comprar !== 1) {
-  numeroElegido = parseInt(prompt('1: 1Kg de milanesas ==> Precio: ' + producto1 + '$ \n2: Alfajor ==> Precio: ' + producto2 + '$ \n3: 1Kg de pollo ==> Precio: ' + producto3 + '$ \n4: Televisor ==> Precio: ' + producto4 + '$ \n5: Camisa Azul ==> Precio: ' + producto5 + '$\nTotal del Carrito = ' + carrito + '$' + '\nEfectivo en mano: 5000$' + '\n6: Comprar'));
-  if (numeroElegido === 1) {
-    carrito = carrito + producto1;
-  } else if (numeroElegido === 2) {
-    carrito = carrito + producto2;
-  } else if (numeroElegido === 3) {
-    carrito = carrito + producto3;
-  } else if (numeroElegido === 4) {
-    carrito = carrito + producto4;
-  } else if (numeroElegido === 5) {
-    carrito = carrito + producto5;
-  } else if (numeroElegido === 6) {
-    comprar++;
-  } else {
-    alert('No ingresó un valor válido');
+class Libros {
+  constructor(nombre, autor, genero, precio) {
+    this.nombre = nombre;
+    this.autor = autor;
+    this.genero = genero;
+    this.precio = precio;
+  }
+  info() {
+    alert('Nombre: ' + this.nombre + '\nAutor: ' + this.autor + '\nGenero: ' + this.genero + '\nPrecio: ' + this.precio);
   }
 }
-function compra(carrito, efectivo) {
-  if (carrito < efectivo) {
-    cambio = efectivo - carrito;
-    return alert('El total de la compra fue ' + carrito + '$ \nLe sobraron ' + cambio + '$');
-  } else if (carrito > efectivo) {
-    return alert('No tiene el dinero suficiente para hacer la compra');
+let carrito = 0;
+let comprar = 0;
+
+libro1 = new Libros('El Archivo de las Tormentas', 'Brandon Sanderson', 'Fantasía', 990);
+libro2 = new Libros('El Problema de los Tres Cuerpos', 'Cixin Liu', 'Ciencia Ficción', 1400);
+libro3 = new Libros('Dune', 'Frank Herbert', 'Ciencia Ficción', 550);
+libro4 = new Libros('Harry Potter', 'J.K Rowling', 'Fantasía', 1000);
+
+librosFantasia = [];
+librosCF = [];
+
+librosFantasia.push(libro1, libro4);
+librosCF.push(libro2, libro3);
+
+function seleccionar(seleccion) {
+  while (seleccion != 0) {
+    seleccion = parseInt(prompt(`Bienvenido a la Libreria PandaPerezoso, seleccione un género de libro para buscar:\n1: Fantasía\n2: Ciencia Ficción\n\n0: Salir`));
+    switch (seleccion) {
+      case 1:
+        seleccionFantasia();
+        break;
+      case 2:
+        seleccionCF();
+        break;
+      case 0:
+        break;
+      default:
+        alert('No ingresó un valor válido');
+        seleccionar();
+        break;
+    }
   }
 }
-compra(carrito, efectivo);
+function seleccionFantasia(libroElegido) {
+  while (libroElegido != 0) {
+    libroElegido = parseInt(prompt(`Estos son los libros de fantasía que tenemos, elija para comprar:\n1-${libro1.nombre}. Precio: $${libro1.precio}\n2-${libro4.nombre}. Precio $${libro4.precio}\n\n3: Ver más info de los libros.\n4: Comprar\nCarrito: $${carrito}\n0: Volver`));
+    switch (libroElegido) {
+      case 1:
+        carrito += libro1.precio;
+        alert(libro1.nombre + ' ha sido agregado al carrito');
+        break;
+      case 2:
+        carrito += libro4.precio;
+        alert(libro4.nombre + ' ha sido agregado al carrito');
+        break;
+      case 3:
+        librosFantasia.forEach((libro) => {
+          libro.info();
+        });
+        break;
+      case 4:
+        alert('El total de la compra fue de $' + carrito + ' ¡Hasta la próxima!');
+        libroElegido = 0;
+        carrito = 0;
+        break;
+      case 0:
+        break;
+      default:
+        alert('No ha ingresado un valor válido');
+        break;
+    }
+  }
+}
+function seleccionCF(libroElegido) {
+  while (libroElegido != 0) {
+    libroElegido = parseInt(prompt(`Estos son los libros de fantasía que tenemos, elija para comprar:\n1-${libro2.nombre}. Precio: $${libro2.precio}\n2-${libro3.nombre}. Precio $${libro3.precio}\n\n3: Ver más info de los libros.\n4: Comprar\nCarrito: $${carrito}\n0: Volver`));
+    switch (libroElegido) {
+      case 1:
+        carrito += libro2.precio;
+        alert(libro2.nombre + ' ha sido agregado al carrito');
+        break;
+      case 2:
+        carrito += libro3.precio;
+        alert(libro3.nombre + ' ha sido agregado al carrito');
+        break;
+      case 3:
+        librosCF.forEach((libro) => {
+          libro.info();
+        });
+        break;
+      case 4:
+        alert('El total de la compra fue de $' + carrito + ' ¡Hasta la próxima!');
+        libroElegido = 0;
+        carrito = 0;
+        break;
+      case 0:
+        break;
+      default:
+        alert('No ha ingresado un valor válido');
+        break;
+    }
+  }
+}
+seleccionar();
