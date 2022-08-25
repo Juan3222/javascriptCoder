41b7f7960e5cbb9e02b731d08d415f751a8a4842
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2022-08-25 12:49:13 -0300

    Creando eventos

--- js/main.js
+++ js/main.js
@@ -1,146 +1,78 @@
 class Libros {
-  constructor(nombre, autor, genero, precio) {
+  constructor(nombre, autor, genero, precio, img, id) {
     this.nombre = nombre;
     this.autor = autor;
     this.genero = genero;
     this.precio = precio;
-  }
-  info() {
-    alert('Nombre: ' + this.nombre + '\nAutor: ' + this.autor + '\nGenero: ' + this.genero + '\nPrecio: ' + this.precio);
+    this.img = img;
+    this.id = id;
   }
 }
 let carrito = 0;
 let comprar = 0;
 
-libro1 = new Libros('El Archivo de las Tormentas', 'Brandon Sanderson', 'Fantasía', 990);
-libro2 = new Libros('El Problema de los Tres Cuerpos', 'Cixin Liu', 'Ciencia Ficción', 1400);
-libro3 = new Libros('Dune', 'Frank Herbert', 'Ciencia Ficción', 550);
-libro4 = new Libros('Harry Potter', 'J.K Rowling', 'Fantasía', 1000);
+libro1 = new Libros('El Camino de los Reyes', 'Brandon Sanderson', 'Fantasía', 990, 'https://www.penguinlibros.com/uy/1765875/el-camino-de-los-reyes-el-archivo-de-las-tormentas-1.jpg', 0);
+libro2 = new Libros('El Problema de los Tres Cuerpos', 'Cixin Liu', 'Ciencia Ficción', 1400, 'https://www.penguinlibros.com/uy/243123/el-problema-de-los-tres-cuerpos-trilogia-de-los-tres-cuerpos-1.jpg', 1);
+libro3 = new Libros('Dune', 'Frank Herbert', 'Ciencia Ficción', 550, 'https://1.bp.blogspot.com/-ZyR9QhRn2MY/XvxFKYjBAFI/AAAAAAAAZPs/WIcAZt8Mkn0YriEGLXWgsRBWOCXlz7ykQCNcBGAsYHQ/s1600/dune.jpg', 2);
+libro4 = new Libros('Harry Potter', 'J.K Rowling', 'Fantasía', 1000, 'https://static.wikia.nocookie.net/esharrypotter/images/d/db/HP1_portada_espa%C3%B1ol_de_bolsillo_2020.jpg', 3);
+libro5 = new Libros('El Bestiario de Axlin', 'Laura Gallego', 'Fantasía', 2000, 'https://images-na.ssl-images-amazon.com/images/I/91XWCsyDYrL.jpg', 4);
+libro6 = new Libros('Fundación', 'Isaac Asimov', 'Ciencie Ficción', 600, 'https://images-na.ssl-images-amazon.com/images/I/81HVLHE1yTL.jpg', 5);
 
-const librosFantasia = [];
-const librosCF = [];
 const librosTotal = [];
+const carro = [];
 
-librosFantasia.push(libro1, libro4);
-librosCF.push(libro2, libro3);
-librosTotal.push(libro1, libro2, libro3, libro4);
+librosTotal.push(libro1, libro2, libro3, libro4, libro5, libro6);
 
-function seleccionar(seleccion) {
-  while (seleccion != 0) {
-    seleccion = parseInt(prompt(`Bienvenido a la Libreria PandaPerezoso, seleccione un género de libro para buscar:\n1: Fantasía\n2: Ciencia Ficción\n\n3: Ver info completa de los libros en la página\n4: Ver info completa de los libros en la página, y ordenados por precio de menor a mayor\n0: Salir`));
-    switch (seleccion) {
-      case 1:
-        seleccionFantasia();
-        break;
-      case 2:
-        seleccionCF();
-        break;
-      case 3:
-        escribirLista();
-        seleccion = 0;
-        break;
-      case 4:
-        ordenarPorPrecio();
-        escribirLista();
-        seleccion = 0;
-      case 0:
-        break;
-      default:
-        alert('No ingresó un valor válido');
-        seleccionar();
-        break;
-    }
-  }
-}
-function seleccionFantasia(libroElegido) {
-  while (libroElegido != 0) {
-    libroElegido = parseInt(prompt(`Estos son los libros de fantasía que tenemos, elija para comprar:\n1-${libro1.nombre}. Precio: $${libro1.precio}\n2-${libro4.nombre}. Precio $${libro4.precio}\n\n3: Ver más info de los libros.\n4: Comprar\nCarrito: $${carrito}\n0: Volver`));
-    switch (libroElegido) {
-      case 1:
-        carrito += libro1.precio;
-        alert(libro1.nombre + ' ha sido agregado al carrito');
-        break;
-      case 2:
-        carrito += libro4.precio;
-        alert(libro4.nombre + ' ha sido agregado al carrito');
-        break;
-      case 3:
-        librosFantasia.forEach((libro) => {
-          libro.info();
-        });
-        break;
-      case 4:
-        alert('Gracias por su compra, salga del programa para ver el total gastado en la página');
-        libroElegido = 0;
-        seleccion = 0;
-        comprarHTML();
-        carrito = 0;
-        break;
-      case 0:
-        break;
-      default:
-        alert('No ha ingresado un valor válido');
-        break;
-    }
-  }
-}
-function seleccionCF(libroElegido) {
-  while (libroElegido != 0) {
-    libroElegido = parseInt(prompt(`Estos son los libros de fantasía que tenemos, elija para comprar:\n1-${libro2.nombre}. Precio: $${libro2.precio}\n2-${libro3.nombre}. Precio $${libro3.precio}\n\n3: Ver más info de los libros.\n4: Comprar\nCarrito: $${carrito}\n0: Volver`));
-    switch (libroElegido) {
-      case 1:
-        carrito += libro2.precio;
-        alert(libro2.nombre + ' ha sido agregado al carrito');
-        break;
-      case 2:
-        carrito += libro3.precio;
-        alert(libro3.nombre + ' ha sido agregado al carrito');
-        break;
-      case 3:
-        librosCF.forEach((libro) => {
-          libro.info();
-        });
-        break;
-      case 4:
-        alert('Gracias por su compra, salga del programa para ver el total gastado en la página');
-        libroElegido = 0;
-        seleccion = 0;
-        comprarHTML();
-        carrito = 0;
-        break;
-      case 0:
-        break;
-      default:
-        alert('No ha ingresado un valor válido');
-        break;
-    }
-  }
-}
-function escribirLista() {
+function mostrarProductos() {
+  let html;
   librosTotal.forEach((libro) => {
-    let contenedor = document.createElement('div');
-    contenedor.innerHTML = `<h3>Nombre del libro:  ${libro.nombre}</h3>
-                            <p>Género del libro: ${libro.genero}</p>
-                            <p>Autor del libro: ${libro.autor}</p>
-                            <p>Precio: ${libro.precio}</p>
-                            <br>`;
-    document.body.append(contenedor);
+    html =
+      html +
+      `
+    <div onclick="agregarCarro(${libro.id})" id = "card">
+      <h3>Nombre del libro:  ${libro.nombre}</h3>
+      <p>Género del libro: ${libro.genero}</p>
+      <p>Autor del libro: ${libro.autor}</p>
+      <p>Precio: ${libro.precio}</p>
+      <img src="${libro.img}" alt="">
+      <br>
+    </div>  
+    `;
   });
+  document.getElementById('productos').innerHTML = html;
 }
-function comprarHTML() {
-  let contenedor = document.createElement('div');
-  contenedor.innerHTML = `<h2>El total de su compra fue de $${carrito}</h2>`;
-  document.body.append(contenedor);
-}
-function ordenarPorPrecio() {
-  librosTotal.sort(function (a, b) {
-    if (a.precio > b.precio) {
-      return 1;
-    }
-    if (a.precio < b.precio) {
-      return -1;
-    }
-    return 0;
+
+function mostrarCarro() {
+  let html;
+  carro.forEach((libro) => {
+    html =
+      html +
+      `
+    <div" id = "card">
+      <h3>Nombre del libro:  ${libro.nombre}</h3>
+      <p>Género del libro: ${libro.genero}</p>
+      <p>Autor del libro: ${libro.autor}</p>
+      <p>Precio: ${libro.precio}</p>
+      <img src="${libro.img}" alt="">
+      <span onclick="eliminarCarro${libro.id}">🛒</span>
+      <br>
+    </div>  
+    `;
   });
+  document.getElementById('carrito').innerHTML = html;
 }
-seleccionar();
+
+function agregarCarro(id) {
+  const resultado = librosTotal.find((el) => el.id === id);
+  carro.push(resultado);
+  mostrarCarro();
+  mostrarProductos();
+}
+function eliminarCarro(id) {
+  const resultado = librosTotal.find((el) => el.id === id);
+  carro.push(resultado);
+  mostrarCarro();
+  mostrarProductos();
+}
+
+mostrarProductos();
