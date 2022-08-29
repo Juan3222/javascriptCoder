ae2bf79f2337389e0d1ff88195d48a56cbf0b588
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2022-08-29 01:26:39 -0300

    Segunda Entrega del Proyecto final

--- js/localStorage.js
+++ js/localStorage.js
@@ -2,6 +2,7 @@ function guardarCarroEnJson(carro) {
   let carroJson = JSON.stringify(carro);
   return localStorage.setItem('carroJson', carroJson);
 }
+
 function obtenerCarroEnJson() {
   carroLs = localStorage.getItem('carroJson');
   carroLs = JSON.parse(carroLs);
