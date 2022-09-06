4bfce13770b98a0e9989553e22b8357e182c100b
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2022-09-06 11:44:03 -0300

    Agregado nuevos libros. Agregado funcionalidades nuevas de ordenamiento. Falta terminar cosas del diseño

--- js/libros.js
+++ js/libros.js
@@ -10,7 +10,9 @@ function mostrarProductos() {
       <p>${libro.autor}</p>
       <p>${libro.genero}</p>
       <p>$${libro.precio},00</p>
-      <button id = "card__button" onclick="agregarCarro(${libro.id})">Agregar al carro</button>
+      <button id = "card__button" onclick="agregarCarro(${libro.id})">
+        <svg id= "card__button--svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g id="card__button--svgColor" fill='#FFFFFF'><path d="M21.96 14.27l2-7c.09-.3.03-.63-.16-.88-.19-.24-.49-.39-.8-.39H5V4c0-.55-.45-1-1-1H1c-.55 0-1 .45-1 1s.45 1 1 1h2v2c0 .01.01.02.01.04 0 .1.02.21.05.3l2.55 7L6.57 17H4c-.55 0-1 .45-1 1s.45 1 1 1h17c.55 0 1-.45 1-1s-.45-1-1-1H8.7l-.73-2H21c.45 0 .84-.3.96-.73zM21.1 10H18V8h3.67l-.57 2zM13 11v2h-3v-2h3zm-3-1V8h3v2h-3zm4 1h3v2h-3v-2zm0-1V8h3v2h-3zM9 8v2H6.15l-.72-2H9zm-2.29 3.52L6.52 11H9v2H7.25l-.54-1.48zM18 13v-2h2.82l-.57 2H18z"></path><circle cx="5" cy="21" r="2"></circle><circle cx="20" cy="21" r="2"></circle></g></svg>
+      </button>
       <br>
     </div>  
     `;
@@ -43,6 +45,49 @@ function filtrarGenero(genero) {
 
     case 'Ciencia Ficción':
       librosTotal = librosTotal.filter((item) => item.genero == genero);
+      break;
+    default:
+      break;
+  }
+  mostrarProductos();
+}
+function filtrarPrecio(ordenar) {
+  switch (ordenar) {
+    case 'Relevancia':
+      librosTotal = [...librosTotalCopia];
+      break;
+    case 'Alfabetico':
+      librosTotal.sort(function (a, b) {
+        if (a.nombre > b.nombre) {
+          return 1;
+        }
+        if (a.nombre < b.nombre) {
+          return -1;
+        }
+        return 0;
+      });
+      break;
+    case 'MenorM':
+      librosTotal.sort(function (a, b) {
+        if (a.precio > b.precio) {
+          return 1;
+        }
+        if (a.precio < b.precio) {
+          return -1;
+        }
+        return 0;
+      });
+      break;
+    case 'MayorM':
+      librosTotal.sort(function (a, b) {
+        if (a.precio > b.precio) {
+          return -1;
+        }
+        if (a.precio < b.precio) {
+          return 1;
+        }
+        return 0;
+      });
       break;
     default:
       break;
--- js/main.js
+++ js/main.js
@@ -9,15 +9,21 @@ class Libros {
   }
 }
 
-libro1 = new Libros(0, 'El Camino de los Reyes', 'Brandon Sanderson', 'Fantasía', 990, 'https://www.penguinlibros.com/uy/1765875/el-camino-de-los-reyes-el-archivo-de-las-tormentas-1.jpg');
-libro2 = new Libros(1, 'El Problema de los Tres Cuerpos', 'Cixin Liu', 'Ciencia Ficción', 1400, 'https://www.penguinlibros.com/uy/243123/el-problema-de-los-tres-cuerpos-trilogia-de-los-tres-cuerpos-1.jpg');
-libro3 = new Libros(2, 'Dune', 'Frank Herbert', 'Ciencia Ficción', 550, 'https://1.bp.blogspot.com/-ZyR9QhRn2MY/XvxFKYjBAFI/AAAAAAAAZPs/WIcAZt8Mkn0YriEGLXWgsRBWOCXlz7ykQCNcBGAsYHQ/s1600/dune.jpg');
-libro4 = new Libros(3, 'Harry Potter y la Piedra Filosofal', 'J.K Rowling', 'Fantasía', 1000, 'https://static.wikia.nocookie.net/esharrypotter/images/d/db/HP1_portada_espa%C3%B1ol_de_bolsillo_2020.jpg');
-libro5 = new Libros(4, 'El Bestiario de Axlin', 'Laura Gallego', 'Fantasía', 2000, 'https://images-na.ssl-images-amazon.com/images/I/91XWCsyDYrL.jpg');
-libro6 = new Libros(5, 'Fundación', 'Isaac Asimov', 'Ciencia Ficción', 600, 'https://images-na.ssl-images-amazon.com/images/I/81HVLHE1yTL.jpg');
-
 let librosTotal = [];
-librosTotal.push(libro1, libro2, libro3, libro4, libro5, libro6);
+librosTotal.push(new Libros(0, 'El Camino de los Reyes', 'Brandon Sanderson', 'Fantasía', 990, 'https://www.penguinlibros.com/uy/1765875/el-camino-de-los-reyes-el-archivo-de-las-tormentas-1.jpg'));
+librosTotal.push(new Libros(1, 'El Problema de los Tres Cuerpos', 'Cixin Liu', 'Ciencia Ficción', 1400, 'https://www.penguinlibros.com/uy/243123/el-problema-de-los-tres-cuerpos-trilogia-de-los-tres-cuerpos-1.jpg'));
+librosTotal.push(new Libros(2, 'Dune', 'Frank Herbert', 'Ciencia Ficción', 550, 'https://1.bp.blogspot.com/-ZyR9QhRn2MY/XvxFKYjBAFI/AAAAAAAAZPs/WIcAZt8Mkn0YriEGLXWgsRBWOCXlz7ykQCNcBGAsYHQ/s1600/dune.jpg'));
+librosTotal.push(new Libros(3, 'Harry Potter y la Piedra Filosofal', 'J.K Rowling', 'Fantasía', 1000, 'https://static.wikia.nocookie.net/esharrypotter/images/d/db/HP1_portada_espa%C3%B1ol_de_bolsillo_2020.jpg'));
+librosTotal.push(new Libros(4, 'El Bestiario de Axlin', 'Laura Gallego', 'Fantasía', 2000, 'https://images-na.ssl-images-amazon.com/images/I/91XWCsyDYrL.jpg'));
+librosTotal.push(new Libros(5, 'Fundación', 'Isaac Asimov', 'Ciencia Ficción', 600, 'https://images-na.ssl-images-amazon.com/images/I/81HVLHE1yTL.jpg'));
+librosTotal.push(new Libros(6, 'El Nombre del Viento', 'Patrick Rothfuss', 'Fantasía', 1990, 'https://images-na.ssl-images-amazon.com/images/I/7125ljaY0gL.jpg'));
+librosTotal.push(new Libros(7, 'Los Pilares de la Tierra', 'Ken Follet', 'Novela histórica', 890, 'https://3.bp.blogspot.com/-3BS_QXV4xOE/Vtdi-es9_XI/AAAAAAAABs0/BbR0Ivfr0YA/s1600/Los%2Bpilares%2Bde%2Bla%2Btierra.jpg'));
+librosTotal.push(new Libros(8, 'La Ciudad de las Bestias', 'Isabel Allende', 'Aventura', 530, 'https://librosya.com.uy/wp-content/uploads/2021/11/9789877254518-scaled.jpg'));
+librosTotal.push(new Libros(9, 'El Imperio Final', 'Brandon Sanderson', 'Fantasía', 1190, 'https://images.cdn3.buscalibre.com/fit-in/360x360/a8/b9/a8b99ba35498b0cc7cfc150cdf00bbc7.jpg'));
+librosTotal.push(new Libros(10, 'Game of Thrones', 'George R.R Martin', 'Fantasía', 1300, 'https://ratondebiblioteca15.files.wordpress.com/2019/03/juego-de-tronos.jpg'));
+librosTotal.push(new Libros(11, 'El Ojo del Mundo', 'Robert Jordan', 'Fantasía', 890, 'https://www.planetadelibros.com.uy/usuaris/libros/fotos/330/m_libros/329536_portada_el-ojo-del-mundo-n-0114_robert-jordan_201910151031.jpg'));
+librosTotal.push(new Libros(12, 'El Mercader de Libros', 'Luis Zueco', 'Novela histórica', 660, 'https://m.media-amazon.com/images/I/51+jPK7pd8L.jpg'));
+// librosTotal.push(new Libros(id, "nombre", "autor", "genero", precio, "img"));
 let librosTotalCopia = [...librosTotal];
 let carro = [];
 
@@ -34,7 +40,7 @@ function numerarCarrito() {
 }
 function searchFilters(input, selector) {
   const d = document;
-  d.addEventListener('input', (e) => {
+  d.addEventListener('keyup', (e) => {
     if (e.target.matches(input)) {
       d.querySelectorAll(selector).forEach((el) => {
         el.textContent.toLowerCase().includes(e.target.value.toLowerCase()) ? el.classList.remove('filtro') : el.classList.add('filtro');
@@ -42,4 +48,4 @@ function searchFilters(input, selector) {
     }
   });
 }
-searchFilters('.buscador', '.card:not(button)');
+searchFilters('.buscador', '.card');
